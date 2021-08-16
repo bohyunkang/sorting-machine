4bb923f08958a9f23ec4cb541f8ecbc253e33955
bohyunkang <bbohyun@kakao.com>
2021-08-16 22:32:07 +0900

    feat : 내림차순 정렬 지연 시간 기능 구현
    Co-authored-by: junamee <skawnkk@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { sortByAscending } from "./sortFunction";
 import "./App.css";
-
 function App() {
+	const [dataToRender, setDataToRender] = useState([]);
 	const [ascendingResult, setAscendingResult] = useState([]);
 	const [descendingResult, setDescendingResult] = useState([]);
 	const inputNumbers = useRef();
 
 	const handleClick = () => {
 		const inputData = inputNumbers.current.value;
-
 		const dataToSort = inputData
 			.trim()
 			.split(",")
 			.filter((el) => !isNaN(el));
 
-		setAscendingResult(sortByAscending(dataToSort));
-		setDescendingResult(sortByAscending(dataToSort).reverse());
+		setDataToRender(dataToSort);
 	};
+
+	useEffect(() => {
+		setAscendingResult(sortByAscending(dataToRender));
+		setTimeout(
+			() => setDescendingResult(sortByAscending(dataToRender).reverse()),
+			3000
+		);
+	}, [dataToRender]);
+
 	return (
 		<div className="App">
 			<div className="sorting-machine section">

3abb3c7beff593000c6aced3fa289e907dd87c07
bohyunkang <bbohyun@kakao.com>
2021-08-16 21:54:03 +0900

    feat : sorting machine 마크업 및 정렬 기능 구현
    Co-authored-by: junamee <skawnkk@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import { sortByAscending } from "./sortFunction";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ascendingResult, setAscendingResult] = useState([]);
+	const [descendingResult, setDescendingResult] = useState([]);
+	const inputNumbers = useRef();
+
+	const handleClick = () => {
+		const inputData = inputNumbers.current.value;
+
+		const dataToSort = inputData
+			.trim()
+			.split(",")
+			.filter((el) => !isNaN(el));
+
+		setAscendingResult(sortByAscending(dataToSort));
+		setDescendingResult(sortByAscending(dataToSort).reverse());
+	};
+	return (
+		<div className="App">
+			<div className="sorting-machine section">
+				<h1>Sorting Machine</h1>
+				<div className="timer-kr section">
+					<span>타이머</span>
+					<div>실제 타이머 영역</div>
+				</div>
+				<div className="number-input section">
+					<span>숫자 입력</span>
+					<input ref={inputNumbers}></input>
+				</div>
+				<button className="start-button section" onClick={handleClick}>
+					정렬 시작
+				</button>
+				<div className="output-asc section">
+					<span>결과 필드 오름차순</span>
+					<div>{ascendingResult.join(", ")}</div>
+				</div>
+				<div className="output-desc section">
+					<span>결과 필드 내림차순</span>
+					<div>{descendingResult.join(", ")}</div>
+				</div>
+				<div className="timer-eu section">
+					<span>타이머</span>
+					<div>실제 타이머 영역</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/sortFunction.js
@@ -0,0 +1,26 @@
+export const sortByAscending = (dataToSort) => {
+	const resultArray = [];
+	//divde
+	if (dataToSort.length < 2) return dataToSort;
+
+	const mid = Math.floor(dataToSort.length / 2);
+	const left = sortByAscending(dataToSort.slice(0, mid));
+	const right = sortByAscending(dataToSort.slice(mid));
+
+	//conquer
+	while (left.length > 0 && right.length > 0) {
+		left[0] < right[0]
+			? resultArray.push(left.shift())
+			: resultArray.push(right.shift());
+	}
+
+	while (left.length === 0 && right.length > 0) {
+		resultArray.push(right.shift());
+	}
+
+	while (left.length > 0 && right.length === 0) {
+		resultArray.push(left.shift());
+	}
+
+	return resultArray;
+};

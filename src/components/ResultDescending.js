469872cec2c724409562ef76a6f24296619e4176
Namju <skawn11114@gmail.com>
2021-08-18 11:05:39 +0900

    refactor : 미사용 프롭 정리

--- src/components/ResultDescending.js
+++ src/components/ResultDescending.js
@@ -1,21 +1,15 @@
-import React from "react";
-import CountDown from "./CountDown";
+import React from 'react';
+import CountDown from './CountDown';
 
 function ResultDescending({ descendingResult, start, setStart }) {
-	return (
-		<div className="output-desc section">
-			<span>내림차순 결과</span>
-			<div className="number-output">
-				<div>
-					{start ? (
-						<CountDown {...{ start, setStart }} />
-					) : (
-						descendingResult.join(", ")
-					)}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='output-desc section'>
+      <span>내림차순 결과</span>
+      <div className='number-output'>
+        <div>{start ? <CountDown setStart={setStart} /> : descendingResult.join(', ')}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ResultDescending;

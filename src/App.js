35ffbaaa7afb6f56275a2230cace37bdd2a5394a
Namju <skawn11114@gmail.com>
2021-08-18 11:06:29 +0900

    chore : prettieir 변경

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
-import { useState, useEffect } from "react";
-import Timer from "./components/Timer";
-import InputNumbers from "./components/InputNumbers";
-import ResultAscending from "./components/ResultAscending";
-import ResultDescending from "./components/ResultDescending";
-import { sortFunction } from "./utils/sortFunction";
-import { MESSAGE } from "./utils/constants";
-import "./App.css";
+import { useState, useEffect } from 'react';
+import Timer from './components/Timer';
+import InputNumbers from './components/InputNumbers';
+import ResultAscending from './components/ResultAscending';
+import ResultDescending from './components/ResultDescending';
+import { sortFunction } from './utils/sortFunction';
+import { MESSAGE } from './utils/constants';
+import './App.css';
 
 function App() {
-	const { GUIDE } = MESSAGE;
-	const [start, setStart] = useState(false);
-	const [dataToRender, setDataToRender] = useState([]);
-	const [ascendingResult, setAscendingResult] = useState([]);
-	const [descendingResult, setDescendingResult] = useState([]);
-	const [message, setMessage] = useState(GUIDE);
+  const [start, setStart] = useState(false);
+  const [dataToRender, setDataToRender] = useState([]);
+  const [ascendingResult, setAscendingResult] = useState([]);
+  const [descendingResult, setDescendingResult] = useState([]);
+  const [message, setMessage] = useState(MESSAGE.GUIDE);
 
-	useEffect(() => {
-		if (dataToRender.length === 0) return;
-		setAscendingResult(sortFunction(dataToRender, "ascending"));
-		setTimeout(
-			() => setDescendingResult(sortFunction(dataToRender, "descending")),
-			3000
-		);
-		setStart(true);
-	}, [dataToRender]);
+  useEffect(() => {
+    if (dataToRender.length === 0) return;
+    setAscendingResult(sortFunction(dataToRender, 'ascending'));
+    setTimeout(() => setDescendingResult(sortFunction(dataToRender, 'descending')), 3000);
+    setStart(true);
+  }, [dataToRender]);
 
-	return (
-		<div className="App">
-			<div className="sorting-machine section">
-				<h1>🤖Sorting Machine🤖</h1>
+  return (
+    <div className='App'>
+      <div className='sorting-machine section'>
+        <h1>🤖 Sorting Machine 🤖</h1>
 
-				<Timer language={"kr-KO"} />
+        <Timer language={'kr-KO'} />
 
-				<InputNumbers {...{ message, setMessage, setDataToRender }} />
-				<ResultAscending ascendingResult={ascendingResult} />
-				<ResultDescending {...{ descendingResult, start, setStart }} />
+        <InputNumbers {...{ message, setMessage, setDataToRender }} />
+        <ResultAscending ascendingResult={ascendingResult} />
+        <ResultDescending {...{ descendingResult, start, setStart }} />
 
-				<Timer language={"en-US"} />
-				<span className="copyright">@bohyunkang & @skawnkk</span>
-			</div>
-		</div>
-	);
+        <Timer language={'en-US'} />
+        <span className='copyright'>@bohyunkang & @skawnkk</span>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/InputNumbers.js
+++ src/components/InputNumbers.js
@@ -1,37 +1,37 @@
-import { useRef } from "react";
-import { MESSAGE } from "../utils/constants";
-import { filterInputs } from "../utils/filterInputs";
+import { useRef } from 'react';
+import { MESSAGE } from '../utils/constants';
+import { filterInputs } from '../utils/filterInputs';
 function InputNumbers({ message, setMessage, setDataToRender }) {
-	const { GUIDE, WRITE, ERROR } = MESSAGE;
-	const inputNumbers = useRef("");
+  const { GUIDE, WRITE, ERROR } = MESSAGE;
+  const inputNumbers = useRef('');
 
-	const handleClick = () => {
-		setMessage(GUIDE);
+  const handleClick = () => {
+    setMessage(GUIDE);
 
-		const inputData = inputNumbers.current.value;
-		if (!inputData) {
-			setMessage(WRITE);
-			return;
-		}
-		const filteredData = filterInputs(inputData);
-		if (filteredData.length === 0) setMessage(ERROR);
-		setDataToRender(filterInputs(inputData));
-	};
+    const inputData = inputNumbers.current.value;
+    if (!inputData) {
+      setMessage(WRITE);
+      return;
+    }
+    const filteredData = filterInputs(inputData);
+    if (filteredData.length === 0) setMessage(ERROR);
+    setDataToRender(filteredData);
+  };
 
-	return (
-		<>
-			<div className="number-input section">
-				<span>{message}</span>
-				<textarea
-					ref={inputNumbers}
-					placeholder="숫자를 콤마(,)로 구분해주세요.&#13;&#10;유효하지 않은 값은 제외됩니다.&#13;&#10;ex) 3,12,7,89,100"
-				/>
-			</div>
-			<button className="start-button section" onClick={handleClick}>
-				정렬 시작하기
-			</button>
-		</>
-	);
+  return (
+    <>
+      <div className='number-input section'>
+        <span>{message}</span>
+        <textarea
+          ref={inputNumbers}
+          placeholder='숫자를 콤마(,)로 구분해주세요.&#13;&#10;유효하지 않은 값은 제외됩니다.&#13;&#10;ex) 3,12,7,89,100'
+        />
+      </div>
+      <button className='start-button section' onClick={handleClick}>
+        정렬 시작하기
+      </button>
+    </>
+  );
 }
 
 export default InputNumbers;

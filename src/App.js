61fe0741a1a3d7c6ec0311ca35c9947a171b7604
Namju <skawn11114@gmail.com>
2021-08-19 15:39:12 +0900

    refactor : useEffect내부에서 비동기 요청 수정
    - async의 위치를 useEffect 내부 함수실행 부분에서 재 작성
    - 즉시실행함수로 작성

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
-import { useState, useEffect } from 'react';
-import Timer from './components/Timer';
-import InputNumbers from './components/InputNumbers';
-import ResultAscending from './components/ResultAscending';
-import ResultDescending from './components/ResultDescending';
-import { sortFunction } from './utils/sortFunction';
-import { MESSAGE } from './utils/constants';
-import { delay } from './utils/delay';
-import './App.css';
+import { useState, useEffect } from "react";
+import InputNumbers from "./components/InputNumbers";
+import SortResult from "./components/SortResult";
+import Timer from "./components/Timer";
+import { sortFunction } from "./utils/sortFunction";
+import { MESSAGE, SORT_TYPE } from "./utils/constants";
+import { delay } from "./utils/delay";
+import "./App.css";
 
 function App() {
+  const { TO_UP, TO_DOWN } = SORT_TYPE;
   const [start, setStart] = useState(false);
   const [dataToRender, setDataToRender] = useState([]);
   const [ascendingResult, setAscendingResult] = useState([]);
   const [descendingResult, setDescendingResult] = useState([]);
   const [message, setMessage] = useState(MESSAGE.GUIDE);
 
-  useEffect(async () => {
+  useEffect(() => {
     if (dataToRender.length === 0) return;
-
     setStart(true);
-    const ascendingSort = await delay(0, sortFunction(dataToRender, 'ascending'));
-    setAscendingResult(ascendingSort);
 
-    const descendingSort = await delay(3000, sortFunction(dataToRender, 'descending'));
-    setDescendingResult(descendingSort);
-  }, [dataToRender]);
+    (async () => {
+      const ascendingSort = await delay(0, sortFunction(dataToRender, TO_UP));
+      setAscendingResult(ascendingSort);
+
+      const descendingSort = await delay(3000, sortFunction(dataToRender, TO_DOWN));
+      setDescendingResult(descendingSort);
+    })();
+  }, [dataToRender, TO_UP, TO_DOWN]);
 
   return (
-    <div className='App'>
-      <div className='sorting-machine section'>
+    <div className="App">
+      <div className="sorting-machine section">
         <h1>🤖 Sorting Machine 🤖</h1>
 
-        <Timer language={'kr-KO'} />
-
+        <Timer language={"kr-KO"} />
         <InputNumbers {...{ message, setMessage, setDataToRender }} />
-        <ResultAscending ascendingResult={ascendingResult} />
-        <ResultDescending {...{ descendingResult, start, setStart }} />
+        <SortResult sortType={TO_UP} sortingResult={ascendingResult} {...{ start, setStart }} />
+        <SortResult sortType={TO_DOWN} sortingResult={descendingResult} {...{ start, setStart }} />
+        <Timer language={"en-US"} />
 
-        <Timer language={'en-US'} />
-        <span className='copyright'>@bohyunkang & @skawnkk</span>
+        <span className="copyright">@bohyunkang & @skawnkk</span>
       </div>
     </div>
   );
